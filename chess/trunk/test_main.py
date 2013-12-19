873ae3985c56f38faa7d8ce324e71a46b1ff74e0
wisedaddy@gmail.com <wisedaddy@gmail.com@98962913-b58d-e65d-faf5-ff428fb360c7>
2013-12-19 22:06:50 +0000

    chess2 is the main chess now, code was beautified

--- chess/trunk/test_main.py
+++ chess/trunk/test_main.py
@@ -1,7 +1,7 @@
 __author__ = 'stafi'
 
 import datetime, sys
-import chess, board, main
+import chess, board
 
 # size_x = 6
 # size_y = 9
@@ -35,6 +35,12 @@ figs = {
 #    chess2.knight: 4
 #}
 
+
+def print_board(brd):
+    for row in brd: print("|".join(row))
+    print("--" * (len(brd)))
+
+
 def main():
     time1 = datetime.datetime.now()
     print("Start time: ", time1)
@@ -42,7 +48,7 @@ def main():
     root_brd = chess.create_empty_board(size_x, size_y)
     brds = chess.place_figs_on_brd(fig_list, root_brd, size_x, size_y)
     time2 = datetime.datetime.now()
-    for brd in brds: main.print_board(chess.unpack_board(brd, size_x, size_y))
+    for brd in brds: print_board(chess.unpack_board(brd, size_x, size_y))
     print("Object size: ", sys.getsizeof(brds))
     print("Number of boards: ", len(brds))
     print("Execution time: ", time2 - time1)
