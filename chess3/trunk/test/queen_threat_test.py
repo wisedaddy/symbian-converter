66149b06e828a0ea3c839fb6d722c3ded189db8b
wisedaddy@gmail.com <wisedaddy@gmail.com@98962913-b58d-e65d-faf5-ff428fb360c7>
2013-12-25 13:22:18 +0000

    chess3 tests fix

--- chess3/trunk/test/queen_threat_test.py
+++ chess3/trunk/test/queen_threat_test.py
@@ -1,72 +1,52 @@
 __author__ = 'stafi'
-import figures, unittest
+import chess3, figures, unittest
 
 class QueenThreatTest(unittest.TestCase):
 
-    def check_threat(self, brd, y, x):
-        return brd[y][x] == board.threat
+
+    def check_threat(self, crds, y, x):
+        return (y, x) in crds
 
 
     def test_p1(self):
         size_x = 4
         size_y = 4
-        brd = chess.create_empty_board(size_y, size_y)
-        self.assertTrue(board.put_queen(brd, 0, 0, size_x, size_y))
-        chess.print_board(brd)
-        self.assertTrue(self.check_threat(brd, 0, 1))
-        self.assertTrue(self.check_threat(brd, 0, 2))
-        self.assertTrue(self.check_threat(brd, 0, 3))
-        self.assertTrue(self.check_threat(brd, 1, 0))
-        self.assertTrue(self.check_threat(brd, 2, 0))
-        self.assertTrue(self.check_threat(brd, 3, 0))
-        self.assertTrue(self.check_threat(brd, 1, 1))
-        self.assertTrue(brd[0][0] == board.queen)
+        crds = figures.queen_threat(0, 0, size_y, size_x)
+        
+        self.assertTrue(self.check_threat(crds, 0, 1))
+        self.assertTrue(self.check_threat(crds, 0, 2))
+        self.assertTrue(self.check_threat(crds, 0, 3))
+        self.assertTrue(self.check_threat(crds, 1, 0))
+        self.assertTrue(self.check_threat(crds, 2, 0))
+        self.assertTrue(self.check_threat(crds, 3, 0))
+        self.assertTrue(self.check_threat(crds, 1, 1))
 
 
     def test_p2(self):
         size_x = 5
         size_y = 5
-        brd = chess.create_empty_board(size_y, size_y)
-        self.assertTrue(board.put_queen(brd, 2, 2, size_x, size_y))
-        chess.print_board(brd)
-        self.assertTrue(self.check_threat(brd, 1, 1))
-        self.assertTrue(self.check_threat(brd, 1, 2))
-        self.assertTrue(self.check_threat(brd, 1, 3))
-        self.assertTrue(self.check_threat(brd, 2, 1))
-        self.assertTrue(self.check_threat(brd, 2, 3))
-        self.assertTrue(self.check_threat(brd, 3, 1))
-        self.assertTrue(self.check_threat(brd, 3, 2))
-        self.assertTrue(self.check_threat(brd, 3, 3))
-        self.assertTrue(self.check_threat(brd, 0, 0))
-        self.assertTrue(self.check_threat(brd, 0, 2))
-        self.assertTrue(self.check_threat(brd, 0, 4))
-        self.assertTrue(self.check_threat(brd, 4, 0))
-        self.assertTrue(self.check_threat(brd, 4, 2))
-        self.assertTrue(self.check_threat(brd, 4, 4))
-        self.assertTrue(self.check_threat(brd, 2, 0))
-        self.assertTrue(self.check_threat(brd, 2, 4))
-        self.assertTrue(brd[0][1] == board.empty)
-        self.assertTrue(brd[0][3] == board.empty)
-        self.assertTrue(brd[4][1] == board.empty)
-        self.assertTrue(brd[4][3] == board.empty)
-
-
-    def test_n1(self):
-        size_x = 4
-        size_y = 4
-        brd = chess.create_empty_board(size_y, size_y)
-        board.put_bishop(brd, 0, 0, size_x, size_y)
-        chess.print_board(brd)
-        self.assertFalse(board.put_queen(brd, 3, 0, size_x, size_y))
-
-
-    def test_n2(self):
-        size_x = 4
-        size_y = 4
-        brd = chess.create_empty_board(size_y, size_y)
-        board.put_rook(brd, 1, 2, size_x, size_y)
-        chess.print_board(brd)
-        self.assertFalse(board.put_queen(brd, 0, 1, size_x, size_y))
+        crds = figures.queen_threat(2, 2, size_y, size_x)
+        
+        self.assertTrue(self.check_threat(crds, 1, 1))
+        self.assertTrue(self.check_threat(crds, 1, 2))
+        self.assertTrue(self.check_threat(crds, 1, 3))
+        self.assertTrue(self.check_threat(crds, 2, 1))
+        self.assertTrue(self.check_threat(crds, 2, 3))
+        self.assertTrue(self.check_threat(crds, 3, 1))
+        self.assertTrue(self.check_threat(crds, 3, 2))
+        self.assertTrue(self.check_threat(crds, 3, 3))
+        self.assertTrue(self.check_threat(crds, 0, 0))
+        self.assertTrue(self.check_threat(crds, 0, 2))
+        self.assertTrue(self.check_threat(crds, 0, 4))
+        self.assertTrue(self.check_threat(crds, 4, 0))
+        self.assertTrue(self.check_threat(crds, 4, 2))
+        self.assertTrue(self.check_threat(crds, 4, 4))
+        self.assertTrue(self.check_threat(crds, 2, 0))
+        self.assertTrue(self.check_threat(crds, 2, 4))
+        self.assertFalse(self.check_threat(crds, 1, 0))
+        self.assertFalse(self.check_threat(crds, 3, 0))
+        self.assertFalse(self.check_threat(crds, 1, 4))
+        self.assertFalse(self.check_threat(crds, 3, 4))
 
 
 if __name__ == '__main__':
